3cc98028a6f732ab36e02dde9cc6d80e919c227c
徐躍華 <joseph981503@gmail.com>
2018-12-05 20:54:49 +0800

    load embedding to dict

--- netquery/bio/train.py
+++ netquery/bio/train.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 
 from netquery.utils import *
-from netquery.bio.data_utils import load_graph
+from netquery.bio.data_utils import load_graph, load_embed
 from netquery.data_utils import load_queries_by_formula, load_test_queries_by_formula
 from netquery.model import QueryEncoderDecoder
 from netquery.train_helpers import run_train
@@ -9,7 +9,7 @@ from netquery.train_helpers import run_train
 from torch import optim
 
 parser = ArgumentParser()
-parser.add_argument("--embed_dim", type=int, default=128)
+parser.add_argument("--embed_dim", type=int, default=64)
 parser.add_argument("--data_dir", type=str, default="./bio_data/")
 parser.add_argument("--lr", type=float, default=0.01)
 parser.add_argument("--depth", type=int, default=0)
@@ -30,7 +30,10 @@ print "Loading graph data.."
 graph, feature_modules, node_maps = load_graph(args.data_dir, args.embed_dim)
 if args.cuda:
     graph.features = cudify(feature_modules, node_maps)
-out_dims = {mode:args.embed_dim for mode in graph.relations}
+out_dims = {mode: args.embed_dim for mode in graph.relations}
+
+print "Loading embed data.."
+id_to_embed = load_embed(args.data_dir)
 
 print "Loading edge data.."
 train_queries = load_queries_by_formula(args.data_dir + "/train_edges.pkl")
@@ -38,12 +41,14 @@ val_queries = load_test_queries_by_formula(args.data_dir + "/val_edges.pkl")
 test_queries = load_test_queries_by_formula(args.data_dir + "/test_edges.pkl")
 
 print "Loading query data.."
-for i in range(2,4):
-    train_queries.update(load_queries_by_formula(args.data_dir + "/train_queries_{:d}.pkl".format(i)))
+for i in range(2, 4):
+    train_queries.update(load_queries_by_formula(
+        args.data_dir + "/train_queries_{:d}.pkl".format(i)))
     i_val_queries = load_test_queries_by_formula(args.data_dir + "/val_queries_{:d}.pkl".format(i))
     val_queries["one_neg"].update(i_val_queries["one_neg"])
     val_queries["full_neg"].update(i_val_queries["full_neg"])
-    i_test_queries = load_test_queries_by_formula(args.data_dir + "/test_queries_{:d}.pkl".format(i))
+    i_test_queries = load_test_queries_by_formula(
+        args.data_dir + "/test_queries_{:d}.pkl".format(i))
     test_queries["one_neg"].update(i_test_queries["one_neg"])
     test_queries["full_neg"].update(i_test_queries["full_neg"])
 
@@ -51,31 +56,33 @@ for i in range(2,4):
 enc = get_encoder(args.depth, graph, out_dims, feature_modules, args.cuda)
 dec = get_metapath_decoder(graph, enc.out_dims if args.depth > 0 else out_dims, args.decoder)
 inter_dec = get_intersection_decoder(graph, out_dims, args.inter_decoder)
-    
+
 enc_dec = QueryEncoderDecoder(graph, enc, dec, inter_dec)
 if args.cuda:
     enc_dec.cuda()
 
 if args.opt == "sgd":
-    optimizer = optim.SGD(filter(lambda p : p.requires_grad, enc_dec.parameters()), lr=args.lr, momentum=0)
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad,
+                                 enc_dec.parameters()), lr=args.lr, momentum=0)
 elif args.opt == "adam":
-    optimizer = optim.Adam(filter(lambda p : p.requires_grad, enc_dec.parameters()), lr=args.lr)
-    
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, enc_dec.parameters()), lr=args.lr)
+
 log_file = args.log_dir + "/{data:s}-{depth:d}-{embed_dim:d}-{lr:f}-{decoder:s}-{inter_decoder:s}.log".format(
-        data=args.data_dir.strip().split("/")[-1],
-        depth=args.depth,
-        embed_dim=args.embed_dim,
-        lr=args.lr,
-        decoder=args.decoder,
-        inter_decoder=args.inter_decoder)
-model_file = args.model_dir + "/{data:s}-{depth:d}-{embed_dim:d}-{lr:f}-{decoder:s}-{inter_decoder:s}.log".format(
-        data=args.data_dir.strip().split("/")[-1],
-        depth=args.depth,
-        embed_dim=args.embed_dim,
-        lr=args.lr,
-        decoder=args.decoder,
-        inter_decoder=args.inter_decoder)
+    data=args.data_dir.strip().split("/")[-1],
+    depth=args.depth,
+    embed_dim=args.embed_dim,
+    lr=args.lr,
+    decoder=args.decoder,
+    inter_decoder=args.inter_decoder)
+model_file = args.model_dir + "/{data:s}-{depth:d}-{embed_dim:d}-{lr:f}-{decoder:s}-{inter_decoder:s}.model".format(
+    data=args.data_dir.strip().split("/")[-1],
+    depth=args.depth,
+    embed_dim=args.embed_dim,
+    lr=args.lr,
+    decoder=args.decoder,
+    inter_decoder=args.inter_decoder)
 logger = setup_logging(log_file)
 
-run_train(enc_dec, optimizer, train_queries, val_queries, test_queries, logger, max_burn_in=args.max_burn_in, val_every=args.val_every, model_file=model_file)
+run_train(enc_dec, optimizer, train_queries, val_queries, test_queries, logger,
+          max_burn_in=args.max_burn_in, val_every=args.val_every, max_iter=args.max_iter, model_file=model_file)
 torch.save(enc_dec.state_dict(), model_file)
